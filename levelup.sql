d134d99f115e8a10981a5db4ffd853df69e68f85
Matthew McMorries <85896879+TomHawk123@users.noreply.github.com>
2022-05-27 14:20:09 -0500

    reports are working

--- levelup.sql
+++ levelup.sql
@@ -1,12 +1,3 @@
-SELECT *
-FROM levelupapi_gametype;
-SELECT *
-FROM auth_user;
-SELECT *
-FROM authtoken_token;
-SELECT *
-FROM levelupapi_gamer;
-DROP TABLE levelupapi_game
 SELECT g.id,
   g.game_type_id,
   g.title,
@@ -18,3 +9,33 @@ SELECT g.id,
 FROM levelupapi_game g
   LEFT JOIN levelupapi_gametype t ON g.game_type_id = t.id
 WHERE t.id = 2
+SELECT le.id,
+  le.date,
+  le.time,
+  lg.title as game_name,
+  au.first_name || " " || au.last_name as full_name
+FROM levelupapi_event le
+  JOIN levelupapi_gamer lgr ON lgr.id = le.organizer_id
+  JOIN auth_user au ON au.id = lgr.user_id
+  JOIN levelupapi_game lg ON le.game_id
+SELECT lg.id,
+  lg.title,
+  lg.maker,
+  lg.skill_level,
+  lg.number_of_players,
+  lg.game_type_id,
+  lg.gamer_id,
+  au.first_name || " " || au.last_name as full_name
+FROM levelupapi_game lg
+  JOIN levelupapi_gamer lgr ON lgr.id = lg.gamer_id
+  JOIN auth_user au ON au.id = lgr.id
+
+SELECT le.id,
+  le.date,
+  le.time,
+  lg.title as game_name,
+  au.first_name || " " || au.last_name as attending_gamer_name
+FROM levelupapi_event le
+  JOIN levelupapi_gamer lgr ON lgr.id = le.organizer_id
+  JOIN auth_user au ON au.id = lgr.user_id
+  JOIN levelupapi_game lg ON le.game_id
--- levelupreports/urls.py
+++ levelupreports/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+
+from levelupreports.views.users.eventsbyuser import UserEventList
 from .views import UserGameList
 
 urlpatterns = [
     path('reports/usergames', UserGameList.as_view()),
+    path('reports/userevents', UserEventList.as_view())
 ]
